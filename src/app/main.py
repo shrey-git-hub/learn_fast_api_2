f27f303270e85a90e2ee4d92d3369ff465c94d6a
Shreyas Employee <shreywankhade.95@gmail.com>
2026-03-18 02:04:31 +0000

    added middleware and ping message

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,18 @@
+from fastapi import FastAPI, Request
+
 app = FastAPI(
     title="My Notes API",
     description="A simple API for managing notes, built with FastAPI.",
     version="1.0.0"
 )
 
+@app.middleware("http")
+async def request_logging_middleware(request: Request, call_next):
+    print(f"INFO: Request: {request.method} {request.url.path}")
+    response = await call_next(request)
+    return response
+
+
+@app.get("/ping")
+def read_root():
+    return {"message": "Pong"}
